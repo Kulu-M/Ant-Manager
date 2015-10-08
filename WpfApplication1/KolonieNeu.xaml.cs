861e79c2e34cbf64d5f265a7569b1e36332efcc7
unknown <lloyd.niebel@googlemail.com>
2015-10-08 19:39:17 +0200

    Save Colonies to XML WIP

--- WpfApplication1/KolonieNeu.xaml.cs
+++ WpfApplication1/KolonieNeu.xaml.cs
@@ -24,7 +24,20 @@ namespace Antspace
         public KolonieNeu()
         {
             InitializeComponent();
+
+            
+
+
         }
+
+        public class Colony {
+
+            public string Cname, Cnote;
+            public object Cart, Cgdatummonth, Cgdatumyear, Agynen, Aworker, Asoldiers;
+            
+
+        }
+
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -34,6 +47,49 @@ namespace Antspace
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void Button_ClickSave(object sender, RoutedEventArgs e)
+        {
+
+            //WIP
+            string CartBox = ArtBox.Get//...
+            string Comboselectedmonth = DateBoxMonth.SelectedItem.ToString();
+            string Comboselectedyear = DateBoxYear.SelectedItem.ToString();
+            string Agynentxt = GyneBox.Text.ToString();
+            string Aworkertxt = WorkerBox.Text.ToString();
+            string Asoldiertxt = SoldierBox.Text.ToString();
+
+
+
+            Colony ColonyOne = new Colony();
+            ColonyOne.Cname = NameBox.Text;
+            ColonyOne.Cart = CartBox;
+            ColonyOne.Cnote = NoteBox.Text;
+
+            ColonyOne.Cgdatummonth = Comboselectedmonth;
+            ColonyOne.Cgdatumyear = Comboselectedyear;
+
+
+            ColonyOne.Agynen = Agynentxt;
+            ColonyOne.Aworker = Aworkertxt;
+            ColonyOne.Asoldiers = Asoldiertxt;
+
+
+
+
+
+            //nn to check if folder exists, "System.IO.Directory.CreateDirectory" does that for us
+            string mydocs = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            System.IO.Directory.CreateDirectory(mydocs + "/AntManager");
+
+            System.Xml.Serialization.XmlSerializer writer = new System.Xml.Serialization.XmlSerializer(typeof(Colony));
+
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/AntManager/AntManagerSaveFile.xml";
+            System.IO.FileStream file = System.IO.File.Create(path);
+
+            writer.Serialize(file, ColonyOne);
+            file.Close();
         }
     }
 }
